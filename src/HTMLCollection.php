6458c5f287ffc641b7bf1ff10e70a437a4494367
DannyS712 <DannyS712.enwiki@gmail.com>
2021-02-05 17:16:41 +0000

    Enable phpcs tests and suppress all failures
    Future patches will enable and fix the exclusions
    The other test scripts are not enabled yet
    
    Two of the failing rules required suppression on a
    file basis instead of repo-wide, no idea while (didn't
    work with either <exclude> or setting the severity to
    0 in the phpcs config file)
    
    Change-Id: Ifbf3eae25d9677e28247081fbdf761362241a035

---
+++ src/HTMLCollection.php
@@ -0,0 +1,3 @@
+<?php
+// phpcs:ignoreFile
+// FIXME this file was empty, these three lines added to pass phpcs, can this file be removed?
--- src/URL.php
+++ src/URL.php
@@ -1,4 +1,5 @@
 <?php
+// phpcs:disable Squiz.Classes.SelfMemberReference.NotUsed
 /******************************************************************************
  * URL
  *
