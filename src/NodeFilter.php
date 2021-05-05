9948113be61c86dcd37536a1af9fda2dfe482ccb
C. Scott Ananian <cscott@cscott.net>
2021-05-05 18:21:21 -0400

    Implement NodeFilter and stub out TreeWalker from IDLeDOM
    Change-Id: Iec9660a0cc0271c24d2ea39a412a0695935c0a1b

--- src/NodeFilter.php
+++ src/NodeFilter.php
@@ -2,13 +2,6 @@
 
 declare( strict_types = 1 );
 
-// phpcs:disable Generic.NamingConventions.CamelCapsFunctionName.MethodDoubleUnderscore
-// phpcs:disable Generic.NamingConventions.CamelCapsFunctionName.ScopeNotCamelCaps
-// phpcs:disable MediaWiki.Commenting.FunctionComment.MissingDocumentationPublic
-// phpcs:disable MediaWiki.Commenting.FunctionComment.WrongStyle
-// phpcs:disable MediaWiki.Commenting.PropertyDocumentation.MissingDocumentationPublic
-// phpcs:disable MediaWiki.Commenting.PropertyDocumentation.WrongStyle
-
 namespace Wikimedia\Dodo;
 
 /**
@@ -18,33 +11,12 @@ namespace Wikimedia\Dodo;
  *
  * @package Wikimedia\Dodo
  */
-abstract class NodeFilter {
-	use UnimplementedTrait;
-
-	// Constants for acceptNode()
-	public const FILTER_ACCEPT = 1;
-	public const FILTER_REJECT = 2;
-	public const FILTER_SKIP = 3;
-
-	// Constants for whatToShow
-	public const SHOW_ALL = 0xFFFFFFFF;
-	public const SHOW_ELEMENT = 0x1;
-	public const SHOW_ATTRIBUTE = 0x2;
-	public const SHOW_TEXT = 0x4;
-	public const SHOW_CDATA_SECTION = 0x8;
-	public const SHOW_ENTITY_REFERENCE = 0x10; // legacy
-	public const SHOW_ENTITY = 0x20; // legacy
-	public const SHOW_PROCESSING_INSTRUCTION = 0x40;
-	public const SHOW_COMMENT = 0x80;
-	public const SHOW_DOCUMENT = 0x100;
-	public const SHOW_DOCUMENT_TYPE = 0x200;
-	public const SHOW_DOCUMENT_FRAGMENT = 0x400;
-	public const SHOW_NOTATION = 0x800; // legacy
+abstract class NodeFilter implements \Wikimedia\IDLeDOM\NodeFilter {
+	// Helper functions from IDLeDOM
+	use \Wikimedia\IDLeDOM\Helper\NodeFilter;
 
 	/**
-	 * @param Node $node
+	 * @inheritDoc
 	 */
-	public function acceptNode( Node $node ) {
-		throw $this->_unimplemented();
-	}
+	abstract public function acceptNode( /* Node */ $node ) : int;
 }
---
+++ src/TreeWalker.php
@@ -0,0 +1,14 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace Wikimedia\Dodo;
+
+abstract class TreeWalker implements \Wikimedia\IDLeDOM\TreeWalker {
+	// Helper functions from IDLeDOM
+	use \Wikimedia\IDLeDOM\Helper\TreeWalker;
+
+	// Stub out methods not yet implemented.
+	use \Wikimedia\IDLeDOM\Stub\TreeWalker;
+	use UnimplementedTrait;
+}
