47073f4d22bd92c48d962012e4809194c40db410
C. Scott Ananian <cscott@cscott.net>
2021-07-03 14:47:23 -0400

    Allow (and ignore) writes to the Element `style` attribute
    This prevents a bunch of spurious failures in the test suites where
    they set/reset various `display` style properties.
    
    Change-Id: I6ede86469de8bbbd9885832889376f3eeee0b9af

--- src/ElementCSSInlineStyle.php
+++ src/ElementCSSInlineStyle.php
@@ -7,4 +7,14 @@ namespace Wikimedia\Dodo;
 trait ElementCSSInlineStyle /* implements \Wikimedia\IDLeDOM\ElementCSSInlineStyle */ {
 	// Stub out methods not yet implemented.
 	use \Wikimedia\IDLeDOM\Stub\ElementCSSInlineStyle;
+
+	/**
+	 * @return CSSStyleDeclaration
+	 */
+	public function getStyle() {
+		// XXX: This is just a stub that will allow (and discard) writes
+		// to the style property.
+		return new CSSStyleDeclaration();
+	}
+
 }
