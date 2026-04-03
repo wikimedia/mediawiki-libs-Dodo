736ffeb38aa5189c4568651e16cd8a8d44045c6f
Umherirrender <umherirrender_de.wp@web.de>
2026-04-04 00:17:51 +0200

    Declare strict types on all php files
    Require strict types on function calls to php internal functions or
    on function calls within the library.
    
    Bug: T422722
    Change-Id: I223fdcdd7433ca0fdaa74c1b02d52e1f48159dce

--- RoboFile.php
+++ RoboFile.php
@@ -1,4 +1,6 @@
 <?php
+declare( strict_types = 1 );
+
 /**
  * This just allows the main robo task file to be properly namespaced!
  */
--- bench/dodo.test.php
+++ bench/dodo.test.php
@@ -1,4 +1,5 @@
 <?php
+declare( strict_types = 1 );
 
 // phpcs:disable Generic.NamingConventions.CamelCapsFunctionName.NotCamelCaps
 // phpcs:disable MediaWiki.Commenting.FunctionComment.WrongStyle
